2506996e6f6a380710fd724b54b06b163ef3192d
Md Jahidul Islam <nabinjahid@gmail.com>
2025-09-07 23:27:06 +0600

    changes in environment file

--- helpers/environments.js
+++ helpers/environments.js
@@ -7,12 +7,14 @@ const environments = {};
 environments.staging = {
   port: 3000,
   envName: "staging",
+  secretKey : 'mySecretKey'
 };
 
 // production environments
 environments.production = {
   port: 5000,
   envName: "production",
+  secretKey : 'mySecretKey'
 };
 
 // determine which environment was passed
